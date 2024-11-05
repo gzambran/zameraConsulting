b69c81a718b4b2dfe1ea3f85c19686c9f6f4f27e
gzambran <gzambran@gmail.com>
2024-11-05 14:44:54 -0500

    Updates

--- README.md
+++
@@ -1,3 +0,0 @@
-# zameraConsulting
-
-[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/gzambran/zameraConsulting)
--- vite.config.ts
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
-export default defineConfig({
-  plugins: [react()],
-  base: '/zameraConsulting/' // Set this to match your GitHub repo name
-});
